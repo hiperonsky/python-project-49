c6abe500bf9764c29147459b675cee5b47bdfe0e
hiperon <hiperonsky@gmail.com>
2023-03-28 23:54:31 +0500

    fixed Makefile for flake8 test and fixed cli.py and brain_games.py after flake8 test

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,9 +2,12 @@
 
 from brain_games.cli import welcome_user
 
-print('Welcome to the Brain Games!') 
+print('Welcome to the Brain Games!')
+
+
 def main():
-    welcome_user() 
-    
+    welcome_user()
+
+
 if __name__ == '__main__':
-     main()
+    main()
