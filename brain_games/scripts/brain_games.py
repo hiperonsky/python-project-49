641fefb8bebd4bc1e3184fe9de25ab31db9c402f
hiperon <hiperonsky@gmail.com>
2023-03-28 18:01:05 +0500

    the end of the 3rd step of the project

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-def main():
-    print('Welcome to the Brain Games!')
+from brain_games.cli import welcome_user
 
+print('Welcome to the Brain Games!') 
+def main():
+    welcome_user() 
+    
 if __name__ == '__main__':
-    main()
+     main()
