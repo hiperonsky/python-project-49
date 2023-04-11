e9f985855cce91e71a992c6f477f9820fee5e9ba
hiperon <hiperonsky@gmail.com>
2023-04-11 16:38:19 +0500

    fixed brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -3,25 +3,28 @@ import prompt
 from brain_games.cli import welcome_user, name
 
 
+def prime_number(number):
+    if number == 1:
+        return 'yes'
+    else:
+        for i in range(2, number):
+            if (number % i) == 0:
+                return 'no'
+                break
+
+
 def main():
     welcome_user()
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     general_counter = 0
     while general_counter < 3:
         number = random.randint(1, 100)
-        true_answer = 'yes'
-        if number == 1:
-            true_answer = 'yes'
-        else:
-            for i in range(2, number):
-                if (number % i) == 0:
-                    true_answer = 'no'
-                    break
+        true_answer = prime_number(number)
         print('Question: ' + str(number))
         user_answer = prompt.string('Your answer: ')
         if str(user_answer) != str(true_answer):
             print(f"'{user_answer}' is wrong answer ;(. \
-                Correct answer was '{str(true_answer)}'.")
+Correct answer was '{str(true_answer)}'.")
             print("Let's try again, " + name + "!")
             break
         else:
