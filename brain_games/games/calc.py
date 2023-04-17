fa601369e08f78beda8eb7fade1bdf0c288c225b
hiperon <hiperonsky@gmail.com>
2023-04-17 16:41:42 +0500

    fixed calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -9,13 +9,13 @@ def game():
     second_number = random.randint(0, 100)
     oper_number = random.randint(1, 3)
     if oper_number == 1:
-        true_answer = first_number + second_number
+        true_answer = str(first_number + second_number)
         exp_char = '+'
     elif oper_number == 2:
-        true_answer = first_number - second_number
+        true_answer = str(first_number - second_number)
         exp_char = '-'
     else:
-        true_answer = first_number * second_number
+        true_answer = str(first_number * second_number)
         exp_char = '*'
-    question = f"{str(first_number)} {exp_char} {str(second_number)}"
+    question = f"{first_number} {exp_char} {second_number}"
     return question, true_answer
