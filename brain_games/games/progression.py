19331931662ae470fdce0509109205efd0d895a6
hiperon <hiperonsky@gmail.com>
2023-04-17 18:19:37 +0500

    fixed progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,31 +4,33 @@ import random
 MAIN_QUESTION = 'What number is missing in the progression?'
 
 
-def progression_find(first_number, second_number):
+def progression_find(initial_term, members_difference):
     counter = 0
     progression = []
     while counter < 10:
-        progression.append(first_number)
-        first_number = first_number + second_number
+        progression.append(initial_term)
+        initial_term = initial_term + members_difference
         counter += 1
     return progression
 
 
-def progression_question(first_number, second_number, prog_random_position):
-    progression = progression_find(first_number, second_number)
+def progression_question(initial_term,
+                         members_difference,
+                         prog_random_position):
+    progression = progression_find(initial_term, members_difference)
     progression[prog_random_position] = '..'
     return " ".join(map(str, progression))
 
 
 def question_answer_generation():
-    first_number = random.randint(0, 100)
-    second_number = random.randint(0, 12)
+    initial_term = random.randint(0, 100)
+    members_difference = random.randint(0, 12)
     prog_random_position = random.randint(0, 9)
     true_answer = str(progression_find(
-        first_number,
-        second_number)[prog_random_position])
+        initial_term,
+        members_difference)[prog_random_position])
     question = str(progression_question(
-        first_number,
-        second_number,
+        initial_term,
+        members_difference,
         prog_random_position))
     return question, true_answer
