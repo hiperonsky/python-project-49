6955aa62ec2d296581f9fa77543c18d117f50ff3
hiperon <hiperonsky@gmail.com>
2023-04-17 18:42:58 +0500

    fixed gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -6,7 +6,7 @@ MAIN_QUESTION = 'Find the greatest common divisor of given numbers.'
 
 def gcd_find(first_number, second_number):
     n = min(first_number, second_number)
-    gcd_result = 0
+    gcd_result = 1
     for i in range(1, n + 1):
         if first_number % i == 0 and second_number % i == 0:
             gcd_result = i
