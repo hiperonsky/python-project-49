f34656cae09b3dbf2592e3f904ad3fd520eadde1
hiperon <hiperonsky@gmail.com>
2023-04-17 17:51:36 +0500

    fixed progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,17 +4,31 @@ import random
 main_question = 'What number is missing in the progression?'
 
 
+def progression_find(first_number, second_number):
+    counter = 0
+    progression = []
+    while counter < 10:
+        progression.append(first_number)
+        first_number = first_number + second_number
+        counter += 1
+    return progression
+
+
+def progression_question(first_number, second_number, prog_random_position):
+    progression = progression_find(first_number, second_number)
+    progression[prog_random_position] = '..'
+    return " ".join(map(str, progression))
+
+
 def game():
     first_number = random.randint(0, 100)
     second_number = random.randint(0, 12)
-    counter = 0
-    list = []
-    while counter < 10:
-        list.append(first_number)
-        first_number = first_number + second_number
-        counter += 1
     prog_random_position = random.randint(0, 9)
-    true_answer = list[prog_random_position]
-    list[prog_random_position] = '..'
-    question = " ".join(map(str, list))
+    true_answer = str(progression_find(
+        first_number,
+        second_number)[prog_random_position])
+    question = str(progression_question(
+        first_number,
+        second_number,
+        prog_random_position))
     return question, true_answer
